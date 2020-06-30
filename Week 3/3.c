6bc420d3cb606882c1f6dfc4c1e4b4ab1644d99f
Nusrat Jahan Marjana <58365562+Nusrat15-12999@users.noreply.github.com>
2020-06-30 23:04:22 +0600

    Create 3.c

---
+++ Week 3/3.c
@@ -0,0 +1,67 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+int Binary_search(int Arr[], int N, int K);
+
+int main()
+{
+
+    int A[50];
+    int n,i,key;
+
+    printf("How many number do you want ???");
+    scanf("%d",& n);
+
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&A[i]);
+    }
+
+    printf("Numbers :");
+
+    for(i=0; i<n; i++)
+    {
+        printf("%d ",A[i]);
+    }
+
+
+    printf("\nKey: ");
+    scanf("%d",&key);
+
+
+	int index = Binary_search(A, n, key);
+
+	if (index != -1)
+    {
+       printf("Found at %d Position", index);
+    }
+	else
+		printf("Not Found");
+
+    getch();
+}
+
+int Binary_search(int Arr[], int N, int K)
+{
+	int low = 0, high = N - 1;
+	int result = -1;
+
+	while (low <= high)
+	{
+		int mid = (low + high)/2;
+
+		if (K ==Arr[mid])
+		{
+			result = mid;
+			high = mid - 1;
+		}
+
+		else if (K < Arr[mid])
+			high = mid - 1;
+
+		else
+			low = mid + 1;
+	}
+	return result;
+}
